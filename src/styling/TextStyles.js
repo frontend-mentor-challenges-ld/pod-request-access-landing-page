a1919f1147e323c8da484a5e7d14362e70a738b9
Lindsey Dortch <codewithlinds@gmail.com>
2025-04-03 22:05:09 -0500

    Created Reusable TextStyles Styled Components

---
+++ src/styling/TextStyles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const TextPreset1 = styled.h1`
+  font-size: 4.8rem;
+  line-height: 120%;
+  text-transform: uppercase;
+  font-weight: var(--fw-bold);
+`;
+
+export const TextPreset2 = styled.p`
+  font-size: 1.8rem;
+  line-height: 150%;
+  font-weight: var(--fw-light);
+`;
+
+export const TextPreset3 = styled.p`
+  font-size: 1.4rem;
+  line-height: 200%;
+  font-weight: var(--fw-light);
+`;
